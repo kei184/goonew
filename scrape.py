9a2a988931c3bd24ff0aa7b7043712c4d16f3686
kei184 <ichihachiyon184@gmail.com>
2025-05-30 14:04:10 +0900

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -8,6 +8,7 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.binary_location = "/usr/bin/google-chrome"
 
 try:
     driver = webdriver.Chrome(options=options)
